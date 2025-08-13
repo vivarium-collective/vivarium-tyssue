e4b2dac927c32ad1e3ff433f43860c70c63ba9c9
sniffo <tasnif.rahman@gmail.com>
2025-08-13 12:41:49 -0400

    initial commit

--- behaviors/behaviors.py
+++ behaviors/behaviors.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-from tyssue.topology.sheet_topology import cell_division, drop_face
+from tyssue.topology.sheet_topology import cell_division, remove_face
 
 def update_stem_cells(eptm):
     eptm.face_df['stem_cell'] = 0
     eptm.face_df['dying_cell'] = 0
-    eptm.face_df.loc[(eptm.face_df["boundary"] == 1) & (eptm.face_df["z"] < 0), "stem_cell"] = 1
-    eptm.face_df.loc[(eptm.face_df["boundary"] == 1) & (eptm.face_df["z"] > 0), "dying_cell"] = 1
-
+    eptm.face_df.loc[(eptm.face_df["boundary"] != 1) & (eptm.face_df["z"] < 0), "stem_cell"] = 1
+    eptm.face_df.loc[(eptm.face_df["z"] > 0), "dying_cell"] = 1
 
 def fix_points_cylinder(sheet, radius):
     xy = sheet.vert_df[['x', 'y']].to_numpy()
@@ -19,7 +18,7 @@ def fix_points_cylinder(sheet, radius):
 
 def divide_cylinder(sheet, manager, geom, division_rate, dt, radius):
     update_stem_cells(sheet)
-    stem_cells = sheet.face_df.loc[sheet.face_df["stem_cell"] == 1]
+    stem_cells = sheet.face_df.loc[(sheet.face_df["stem_cell"] == 1) & (sheet.face_df["area"]>=0.7*sheet.face_df["area"].mean())].copy()
     n_stem = len(stem_cells)
     cell_ids = list(stem_cells["unique_id"])
     n_divisions = np.random.binomial(n=n_stem, p=division_rate * dt)
@@ -41,7 +40,10 @@ def apoptosis_cylinder(sheet, manager, death_rate, dt, radius, geom):
 
     for cell_id in to_kill:
         cell_idx = int(sheet.face_df[sheet.face_df["unique_id"] == cell_id].index[0])
-        vertex = drop_face(sheet, cell_idx, geom)
+        # if sheet.face_df.loc[cell_idx, "boundary"] == 0:
+        vertex = remove_face(sheet, cell_idx)
+        # else:
+        #     vertex = drop_face(sheet, cell_idx, geom)
         # split_vert(sheet, vertex)
-    manager.append(apoptosis_cylinder, death_rate=death_rate, dt=dt, radius=radius)
+    manager.append(apoptosis_cylinder, death_rate=death_rate, dt=dt, radius=radius, geom=geom)
     fix_points_cylinder(sheet, radius=radius)
